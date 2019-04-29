489f443f516e335ca4b08d76d40a67168c95724b
Mike Roberts <mike.roberts@easymarkit.com>
2019-04-28 21:04:14 -0700

    db creds

--- main/settings.py
+++ main/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'Aurelion.mysql.pythonanywhere-services.com',
+        'USER': 'Aurelion',
+        'PASSWORD': 'proth314',
+        'DATABASE': 'Aurelion$foosbot',
+        'PORT': '3306',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
