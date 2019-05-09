3892fb445e00e43950922d60b00006b427f2ca64
Mike Roberts <mike.roberts@easymarkit.com>
2019-05-08 21:36:07 -0700

    leaderboard

--- foosbot/modules/leaderboard.py
+++ foosbot/modules/leaderboard.py
@@ -8,28 +8,37 @@ def get_details(account_id, player_id):
 
     matches = db.table('matches').where(
         db.query().where('player1', player['id']).or_where('player2', player['id'])
-    ).order_by('created_at', 'desc').limit(25).get()
+    )\
+    .where('status', 'complete')\
+    .order_by('created_at', 'desc').limit(25).get()
 
     #add names for displaying
     pname = PlayerName()
+    longest_streak = 0
+    streak = 0
     for match in matches:
         player2 = match.player1 if match.player1 != player['id'] else match.player2
         if match['winner'] == player['id']:
             match['winner_name'] = player['fname']
             match['loser_name'] = pname.getName(player2)
+            streak += 1
         else:
             match['winner_name'] = pname.getName(player2)
             match['loser_name'] = player['fname']
+            streak = 0
+
         match['points'] = int(match['points'])
         match['created_at'] = match['created_at'][:11]
+
+        if streak > longest_streak: longest_streak = streak
     
 
     data = {
         'player':player, 
         'matches':matches, 
-        'total_played': 10, 
-        'total_won': 9, 
-        'longest_streak': 5,
+        'total_played': len(matches), 
+        'total_won': len([m for m in matches if m['winner']==player['id']]), 
+        'longest_streak': longest_streak,
         'account_id':account_id
     }
 
